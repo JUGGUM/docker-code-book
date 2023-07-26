a33028c81d5d0228af79cd428dfb2c79fd8cc8cc
JUGGUM <gayoung801@naver.com>
2023-07-26 16:11:22 +0900

    Test :  DockerCodeBookApplicationTests 생성

---
+++ src/test/java/com/example/dockercodebook/DockerCodeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockercodebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCodeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
