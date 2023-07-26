c345e4b2de331bdd08bed127212e4ce06ed01766
JUGGUM <gayoung801@naver.com>
2023-07-26 14:55:53 +0900

    Feat : DockerCodeBookAppilication 생성

---
+++ src/main/java/com/example/dockercodebook/DockerCodeBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockercodebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerCodeBookApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DockerCodeBookApplication.class, args);
+  }
+
+}
